7280181bbd5bfd87e637c1eebdf9df92ba1fe341
pamelatsabou <99961651+pamelatsabou@users.noreply.github.com>
2022-03-22 23:24:38 +0100

    Day2

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-# Band-name-generator
+# tip calculator
 
-print("Welcome to the Band Name Generator.")
-city_name = input("What name of the city you grew up in?\n")
-pet_name = input("What's your pet's name?\n")
-print(f"Your band name could be {city_name} {pet_name}")
+print("Welcome to the Tip Calculator.")
+total_bill = float(input("What was the total bill? $"))
+percentage_tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+num_of_people = int(input("How many people to split the bill? "))
+tip_as_percent = percentage_tip / 100
+pay_per_person = round(((tip_as_percent * total_bill) + total_bill) / num_of_people, 2)
+final_amount = "{:.2f}".format(pay_per_person)
+print(f"Each person should pay ${final_amount}")
