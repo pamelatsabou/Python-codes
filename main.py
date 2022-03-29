3a08d96c89ec8222e00501619ebc28c83a1c9f32
pamelatsabou <99961651+pamelatsabou@users.noreply.github.com>
2022-03-29 21:20:38 +0100

    Day6

--- main.py
+++ main.py
@@ -1,65 +1,62 @@
-# Password Generator
-
-import random
-
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-print("Welcome to the Password Generator!")
-num_letters = int(input(f"How many letters would you like in your password?\n"))
-num_symbols = int(input(f"How many symbols would you like?\n"))
-num_numbers = int(input(f"How many numbers would you like?\n"))
-
-# Easy level; where the letters are seperated from the numbers and symbols
-'''letters_chosen = ''
-numbers_chosen = ''
-symbols_chosen = ''
-
-for _ in range(0, num_letters):
-    chosen_letter = random.choice(letters)
-    letters_chosen += chosen_letter
-
-for _ in range(0, num_numbers):
-    chosen_number = random.choice(numbers)
-    numbers_chosen += chosen_number
-
-for _ in range(0, num_symbols):
-    chosen_symbol = random.choice(symbols)
-    symbols_chosen += chosen_symbol
-
-print(f"{letters_chosen}{numbers_chosen}{symbols_chosen}")
+# Hurdles loop challenge
 '''
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+for step in range(1, 7):
+    move()
+    jump()
+'''
+# Hurdle3 while loop with if statement
 
-# Difficult level; where the password has all characters mixed
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
-password = ""
-order = ['chosen_letter', 'chosen_number', 'chosen_symbol']
-total_length = num_numbers + num_symbols + num_letters
-count_letters = 0
-count_numbers = 0
-count_symbols = 0
+def jump():
+    turn_left()
+    while wall_on_right() == True:
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear() == True:
+        move()
+    turn_left()
 
-for _ in range(0, total_length):
-    chosen_letter = random.choice(letters)
-    chosen_number = random.choice(numbers)
-    chosen_symbol = random.choice(symbols)
-    chosen_character = random.choice(order)
-    if chosen_character == 'chosen_letter' and num_letters >= count_letters:
-        password += chosen_letter
-        count_letters += 1
-    elif chosen_character == 'chosen_number' and num_numbers >= count_numbers:
-        password += chosen_number
-        count_numbers += 1
-    elif chosen_character == 'chosen_symbol' and num_symbols >= count_symbols:
-        password += chosen_symbol
-        count_symbols
-
-print(f"Your password is {password}")
+while at_goal() == False:
+    if wall_in_front() == True:
+        jump()
+    else:
+        move()
 
 
+# Maze
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
 
+while front_is_clear():
+    move()
+turn_left()
 
-
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+    if front_is_clear():
+        move()
+    else:
+        turn_left()
