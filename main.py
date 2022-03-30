ec475a168c8ef0877afc09dab65f2053dc1a65c8
pamelatsabou <99961651+pamelatsabou@users.noreply.github.com>
2022-03-30 20:22:12 +0100

    Day9

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
-# Caesar Cipher
+# Grading Program
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
 
-decision = input("Do you want to encrypt or decrypt? ")
-word = input("Enter a word: ")
-shift = int(input("By how many places should the letters be shifted? "))
-
-
-def encrypt(shift_number, text):
-    new_word = ""
-    for char in text:
-        position = alphabet.index(char)
-        new_position = position + shift_number
-        new_letter = alphabet[new_position]
-        new_word += new_letter
-    print(f"The encrypted word is {new_word}")
-
-
-def decrypt(shift_number, new_word):
-    text = ""
-    for char in new_word:
-        position = alphabet.index(char)
-        new_position = position - shift_number
-        new_letter = alphabet[new_position]
-        text += new_letter
-    print(f"The decrypted word is {text}")
+student_grades = {}
+for student in student_scores:
+    if student_scores[student] >= 91:
+        student_grades[student] = "Outstanding"
+    elif student_scores[student] >= 81:
+        student_grades[student] = "Exceeds Expectations"
+    elif student_scores[student] >= 71:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+print(student_grades)
 
 
-if decision == 'encode':
-    encrypt(shift, word)
-elif decision == 'decode':
-    decrypt(shift, word)
+# Secret Auction Program
+
+bidders = {}
+turns = 1
+other_bidder_exists = True
+
+
+def secret_auction(name_of_bidder, bid_amount):
+    highest_bid = 0
+    '''Formats the names and bids entered into a dictionary'''
+    for _ in range(turns):
+        bidders[name_of_bidder] = bid_amount
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+#    print(bidders) to check code
+    print(f"The winner is {name} with a bid of ${highest_bid}")
+
+
+while other_bidder_exists:
+    name = input("Enter your name: ")
+    bid = int(input("What is your bid? $"))
+    answer = input("Is there another bidder? ")
+
+    turns += 1
+
+    if answer == 'no':
+        other_bidder_exists = False
+        secret_auction(name, bid)
