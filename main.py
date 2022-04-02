05b367a8cae2de2fd000151d698a54900759a9f0
pamelatsabou <99961651+pamelatsabou@users.noreply.github.com>
2022-04-02 23:13:05 +0100

    Day12

--- main.py
+++ main.py
@@ -1,51 +1,44 @@
-# Grading Program
+# Number Guessing Game
 
-student_scores = {
-    "Harry": 81,
-    "Ron": 78,
-    "Hermione": 99,
-    "Draco": 74,
-    "Neville": 62,
-}
+import random
 
-student_grades = {}
-for student in student_scores:
-    if student_scores[student] >= 91:
-        student_grades[student] = "Outstanding"
-    elif student_scores[student] >= 81:
-        student_grades[student] = "Exceeds Expectations"
-    elif student_scores[student] >= 71:
-        student_grades[student] = "Acceptable"
-    else:
-        student_grades[student] = "Fail"
-print(student_grades)
+play_again = True
 
+while play_again:
+    number = random.choice(range(1, 101))
+    is_over = False
+    print("Welcome to the Number Guessing Game!\n"
+          "I'm thinking of a number between 1 and 100.")
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if difficulty == 'easy':
+        lives = 10
+    elif difficulty == 'hard':
+        lives = 5
 
-# Secret Auction Program
+    def compare(user_guess, true_number):
+        global lives
+        global is_over
+        if user_guess == true_number:
+            is_over = True
+            return "You guessed the number, you win"
+        elif lives == 0:
+            is_over = True
+            return "You ran out of guesses, you lose"
 
-bidders = {}
-turns = 1
-other_bidder_exists = True
+        elif user_guess < true_number:
+            return "Too low.\nGuess again."
+        elif user_guess > true_number:
+            return "Too high.\nGuess again."
 
+    while not is_over:
+        print(f"You have {lives} attempts remaining to guess the number.")
+        lives -= 1
+        guess = int(input("Make a guess: "))
+        print(compare(guess, number))
+        if lives == 0:
+            is_over = True
 
-def secret_auction(name_of_bidder, bid_amount):
-    highest_bid = 0
-    '''Formats the names and bids entered into a dictionary'''
-    for _ in range(turns):
-        bidders[name_of_bidder] = bid_amount
-        if bid_amount > highest_bid:
-            highest_bid = bid_amount
-#    print(bidders) to check code
-    print(f"The winner is {name} with a bid of ${highest_bid}")
+    answer = input("Would you like to play another game? Type 'y' or 'n': ")
+    if answer == 'n':
+        play_again = False
 
-
-while other_bidder_exists:
-    name = input("Enter your name: ")
-    bid = int(input("What is your bid? $"))
-    answer = input("Is there another bidder? ")
-
-    turns += 1
-
-    if answer == 'no':
-        other_bidder_exists = False
-        secret_auction(name, bid)
