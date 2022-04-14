eb618987522856a61dce87d7db22897d2f1df33f
pamelatsabou <99961651+pamelatsabou@users.noreply.github.com>
2022-04-14 21:22:34 +0100

    Day20_21

--- main.py
+++ main.py
@@ -1,55 +1,50 @@
-# Turtle: listening keys from keyboard
+# Snake game
 
-from turtle import Turtle, Screen
+from scoreboard import ScoreBoard
+from food import Food
+from turtle import Screen
+from snake import Snake
+import time
 
-pam = Turtle()
 screen = Screen()
-pam.speed("fastest")
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
 
-'''
-def move_forward():
-    pam.forward(10)
+screen.update()
 
-
-screen.listen()
-screen.onkey(key="space", fun=move_forward)
-'''
-
-
-def move_forwards():
-    pam.forward(50)
-
-
-def move_backwards():
-    pam.back(50)
-
-
-def move_counter_clockwise():
-    new_heading = pam.heading() + 10
-    pam.setheading(new_heading)
-
-
-def draw_circle():
-    pam.circle(50, 45, 5)
-
-
-def move_clockwise():
-    new_heading = pam.heading() - 10
-    pam.setheading(new_heading)
-
-
-def clear_drawing():
-    pam.clear()
-    pam.reset()
-
+score = ScoreBoard()
+food = Food()
+snake = Snake()
 
 screen.listen()
-screen.onkey(key="W", fun=move_forwards)
-screen.onkey(key="S", fun=move_backwards)
-screen.onkey(key="A", fun=move_counter_clockwise)
-screen.onkey(key="D", fun=move_clockwise)
-screen.onkey(key="C", fun=clear_drawing)
-screen.onkey(key="Q", fun=draw_circle)
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 300 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
+        game_is_on = False
+        score.game_over()
+
+    # Detect collision tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
 
 screen.exitonclick()
