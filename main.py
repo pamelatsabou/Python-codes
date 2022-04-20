83ae6701ca0ce9a6e1820f9f8e31cf307f4b6a25
pamelatsabou <99961651+pamelatsabou@users.noreply.github.com>
2022-04-20 21:49:08 +0100

    Day23_TurtleCrossingGame

--- main.py
+++ main.py
@@ -1,64 +1,40 @@
-# Arcade game
-
-from turtle import Turtle, Screen
-from paddle import Paddle
-from ball import Ball
-from scoreboard import ScoreBoard
 import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import ScoreBoard
 
 screen = Screen()
-screen.screensize(canvwidth=800, canvheight=600, bg="black")
-screen.title("Pong Game")
+screen.setup(width=600, height=600)
 screen.tracer(0)
 
-pam = Turtle()
-pam.speed("fastest")
-pam.color("white")
-pam.hideturtle()
-pam.penup()
-pam.goto(0, -250)
-pam.left(90)
-
-for _ in range(34):
-    pam.pensize(3)
-    pam.pendown()
-    pam.forward(5)
-    pam.penup()
-    pam.forward(10)
-
-l_paddle = Paddle((-320, 0))
-r_paddle = Paddle((320, 0))
-score = ScoreBoard()
-ball = Ball()
+pam = Player()
+car_manager = CarManager()
+scoreboard = ScoreBoard()
 
 screen.listen()
-screen.onkey(r_paddle.up, "Up")
-screen.onkey(r_paddle.down, "Down")
-screen.onkey(l_paddle.up, "w")
-screen.onkey(l_paddle.down, "s")
+screen.onkey(pam.move_forwards, "Up")
+# screen.onkey(pam.move_backwards, "Down")
+
 
 game_is_on = True
 while game_is_on:
-    time.sleep(ball.move_speed)
+    time.sleep(0.1)
     screen.update()
-    ball.move_ball()
 
-    # Detect collision with the wall
-    if ball.ycor() >= 270 or ball.ycor() <= -270:
-        ball.bounce_vertically()
+    car_manager.create_car()
+    car_manager.car_movements()
 
-    # Detect collision with r_paddle
-    if (ball.distance(r_paddle) <= 50 and ball.xcor() > 300) or (ball.distance(l_paddle) <= 50 and ball.xcor() < -300):
-        ball.bounce_horizontally()
+    # Detect collision with cars
+    for car in car_manager.all_cars:
+        if car.distance(pam) < 20:
+            game_is_on = False
+            scoreboard.game_over()
 
-    # Detect r_paddle misses
-    if ball.xcor() > 330:
-        ball.reset_position()
-        score.l_point()
-
-    # Detect l_paddle misses
-    if ball.xcor() < -330:
-        ball.reset_position()
-        score.r_point()
+    # Detect successful crossing
+    if pam.is_at_finish_line():
+        pam.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
 
 screen.exitonclick()
