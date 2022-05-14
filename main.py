f4fae7b7cc6e24ed531e6d424081f95cb678e7d4
pamelatsabou <99961651+pamelatsabou@users.noreply.github.com>
2022-05-14 22:00:41 +0100

    Day26

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-import turtle
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+# Looping through dictionaries:
+for (key, value) in student_dict.items():
+    # Access key and value
+    pass
+
 import pandas
 
-screen = turtle.Screen()
-screen.title("U.S. States Game")
-image = "blank_states_img.gif"
-screen.addshape(image)
-turtle.shape(image)
+student_data_frame = pandas.DataFrame(student_dict)
 
-data = pandas.read_csv("50_states.csv")
-states = data.state.to_list()
-guessed_states = []
+# Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    # Access index and row
+    # Access row.student or row.score
+    pass
 
-while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
-                                    prompt="What's another state's name?").title()
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
 
-    if answer_state == "Exit":
-        df = pandas.DataFrame(states)
-        df.to_csv("missing states.csv")
-        break
-    if answer_state in states:
-        guessed_states.append(answer_state)
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.penup()
-        answer_row = data[data.state == answer_state]
-        t.goto(int(answer_row.x), int(answer_row.y))
-        t.write(answer_state)
-        states.remove(answer_state)
-
-turtle.mainloop()
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+alphabet_csv = pandas.read_csv("nato_phonetic_alphabet.csv")
+alphabet_data_frame = pandas.DataFrame(alphabet_csv)
+# print(alphabet_data_frame)
+alphabet_dict = {row.letter: row.code for (index, row) in alphabet_data_frame.iterrows()}
+# print(alphabet_dict)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+word_list = [alphabet_dict[letter] for letter in word]
+print(word_list)
