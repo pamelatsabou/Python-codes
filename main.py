5af0ae1093cb972f9edc9e2e14df288f58f1e9d1
pamelatsabou <99961651+pamelatsabou@users.noreply.github.com>
2022-06-22 00:16:12 +0100

    Day31

--- main.py
+++ main.py
@@ -1,70 +1,66 @@
-# FileNotFound
-'''
+from tkinter import *
+import pandas
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+words = {}
+
 try:
-    file = open("my_file.txt")
-    a_dictionary = {'key': "value"}
-    print(a_dictionary["key"])
+    data = pandas.read_csv("data/words_to_learn.csv")
 except FileNotFoundError:
-    file = open("my_file.txt", "w")
-    file.write("Something")
-except KeyError as error_answer:
-    print(f"The key {error_answer} does not exist.")
+    original_data = pandas.read_csv("data/french_words.csv")
+    words = original_data.to_dict(orient="records")
 else:
-    content = file.read()
-    print(content)
-finally:
-    raise KeyError("This is an error that I made")
-'''
-
-###########    BMI      ###########
-
-'''
-height = float(input("Height: "))
-weight = float(input("Weight: "))
-
-if height > 3:
-    raise ValueError("Human height should not be over 3 meters.")
-
-bmi = weight / height ** 2
-print(bmi)
-'''
-
-#########     Coding challenge1     ######
-
-'''
-fruits = ["Apple", "Pear", "Orange"]
+    words = data.to_dict(orient="records")
 
 
-def make_pie(index):
-    try:
-        fruit = fruits[index]
-    except IndexError:
-        fruit = "Fruit"
-    print(fruit + " pie")
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(words)
+    card_front.itemconfig(card_title, text="French", fill="black")
+    card_front.itemconfig(card_word, text=current_card["French"], fill='black')
+    card_front.itemconfig(card_background, image=image1)
+    window.after(3000, func=flip_card)
 
 
-make_pie(4)
+def flip_card():
+    card_front.itemconfig(card_title, text="English")
+    card_front.itemconfig(card_word, text=current_card["English"], fill="white")
+    card_front.itemconfig(card_background, image=card_back_img)
 
-'''
 
-#########     Coding challenge2     ######
+def is_known():
+    words.remove(current_card)
+    next_card()
+    data = pandas.DataFrame(words)
+    data.to_csv("data/words to learn.csv", index=False)
 
-facebook_posts = [
-    {'Likes': 21, 'Comments': 2},
-    {'Likes': 13, 'Comments': 2, 'Shares': 1},
-    {'Likes': 33, 'Comments': 8, 'Shares': 3},
-    {'Comments': 4, 'Shares': 2},
-    {'Comments': 1, 'Shares': 1},
-    {'Likes': 19, 'Comments': 3},
-]
 
-total_likes = 0
+window = Tk()
+window.title("Flashy")
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
-for post in facebook_posts:
-    try:
-        total_likes = total_likes + post['Likes']
-    except KeyError:
-        post['Likes'] = 0
-        total_likes = total_likes + post['Likes']
+flip_timer = window.after(3000, func=flip_card)
 
-print(total_likes)
+card_front = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+image1 = PhotoImage(file="/home/pamela/Documents/Python projects/1.1 flash-card-project-start/images/card_front.png")
+card_back_img = PhotoImage(file="/home/pamela/Documents/Python projects/1.1 flash-card-project-start/images/card_back.png")
+
+card_background = card_front.create_image(400, 263, image=image1)
+card_title = card_front.create_text(400, 150, text='', font=('Arial', 40, "italic"))
+card_word = card_front.create_text(400, 250, text="", font=("Arial", 60, "bold"))
+card_front.grid(column=0, row=0, columnspan=2)
+
+check_image = PhotoImage(file="/home/pamela/Documents/Python projects/1.1 flash-card-project-start/images/wrong.png")
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button.grid(column=0, row=1)
+
+cross_image = PhotoImage(file="/home/pamela/Documents/Python projects/1.1 flash-card-project-start/images/right.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=is_known)
+unknown_button.grid(column=1, row=1)
+
+next_card()
+
+window.mainloop()
